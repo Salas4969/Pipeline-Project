edf75e31fbf77bc8843cda646b606e13a038841e
Salas4969 <82052286+Salas4969@users.noreply.github.com>
2023-12-06 13:19:53 -0500

    Update PipelinCode.java

--- PipelinCode.java
+++ PipelinCode.java
@@ -1,24 +1,69 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.zip.GZIPInputStream;
+import java.util.Map;
 
-public class WordFrequencyCounter {
+public class WordFrequencyHistogram {
+
     public static void main(String[] args) {
-        try {
-            // Step 1: Decompress the gzipped XML file
-            String gzippedFilePath = "aawiki-20231001-pages-logging.xml.gz";
-            GZIPInputStream gzipInputStream = new GZIPInputStream(new FileInputStream(gzippedFilePath));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(gzipInputStream));
+        // Step 1: Read the text from a file
+        String filePath = "aawiki-20231001-pages-logging.xml.gz";
+        String text = readTextFromFile(filePath);
 
-            // Step 2: Read and tokenize XML content
-            StringBuilder content = new StringBuilder();
+        // Step 2: Count word frequency
+        Map<String, Integer> wordFrequency = countWordFrequency(text);
+
+        // Step 3: Display word counts
+        System.out.println("Word Counts:");
+        for (Map.Entry<String, Integer> entry : wordFrequency.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        // Step 4: Draw histogram
+        drawHistogram(wordFrequency);
+    }
+
+    private static String readTextFromFile(String filePath) {
+        StringBuilder text = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
+                text.append(line).append(" ");
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text.toString();
+    }
 
-            // Step 3: Tokenize and count word occurrences
-            String[] words = content.
+    private static Map<String, Integer> countWordFrequency(String text) {
+        Map<String, Integer> wordFrequency = new HashMap<>();
+        String[] words = text.split("\\s+");
+
+        for (String word : words) {
+            // Remove non-alphabetic characters and convert to lowercase
+            word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+            // Update word frequency
+            wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
+        }
+
+        return wordFrequency;
+    }
+
+    private static void drawHistogram(Map<String, Integer> wordFrequency) {
+        System.out.println("\nWord Frequency Histogram:");
+
+        for (Map.Entry<String, Integer> entry : wordFrequency.entrySet()) {
+            String word = entry.getKey();
+            int frequency = entry.getValue();
+
+            System.out.print(word + ": ");
+            for (int i = 0; i < frequency; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
